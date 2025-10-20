5376821653931310605e6a31c86b38eb587bf004
Jesse Slicer <jcslicer@gmail.com>
2025-10-20 15:35:28 -0500

    Updated NuGet references, updated version numbers, and made sure coverlet was configured.

---
+++ Murmur3.Tests/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Always Elucidated Solution Pioneers, LLC">
+//   Copyright (c) Always Elucidated Solution Pioneers, LLC. All rights reserved.
+// </copyright>
+// <summary>
+//   Assembly info.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+[assembly: Parallelize]
