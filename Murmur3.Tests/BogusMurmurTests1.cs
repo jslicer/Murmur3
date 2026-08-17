d8e64d3e89b41be065851b016f37a83fedf735f1
Jesse Slicer <jcslicer@gmail.com>
2026-08-17 14:58:14 -0500

    Imma keep burning up GitHub's CPUs until I get something useful.

--- Murmur3.Tests/BogusMurmurTests1.cs
+++ Murmur3.Tests/BogusMurmurTests1.cs
@@ -56,9 +56,8 @@ public sealed class BogusMurmurTests1 : Murmur3TestsBase
     /// value.</exception>
     /// <exception cref="FormatException">value does not comply with the input pattern specified by style.</exception>
     [TestMethod]
-    public void EnsureHashOfNullCaught() =>
-        _ = Assert.ThrowsExactly<InvalidOperationException>(() =>
-            TestSmHasher("The quick brown fox jumps over the lazy dog"));
+    public void EnsureHashOfNullCaught() => _ = Assert.ThrowsExactly<InvalidOperationException>(() => TestSmHasher(
+        "The quick brown fox jumps over the lazy dog"));
 
     /// <inheritdoc />
     /// <summary>
