340e144526a92868e88a0f564e7707ccd5b504bc
Jesse Slicer <jcslicer@gmail.com>
2026-08-17 15:00:41 -0500

    The beat goes on...

--- Murmur3.Tests/BogusMurmurTests2.cs
+++ Murmur3.Tests/BogusMurmurTests2.cs
@@ -56,8 +56,13 @@ public sealed class BogusMurmurTests2 : Murmur3TestsBase
     /// value.</exception>
     /// <exception cref="FormatException">value does not comply with the input pattern specified by style.</exception>
     [TestMethod]
-    public void EnsureBadConstructorCaught() => _ = Assert.ThrowsExactly<MissingMethodException>(() => TestSmHasher(
-        "The quick brown fox jumps over the lazy dog"));
+    public void EnsureBadConstructorCaught()
+    {
+#pragma warning disable IDE0022 // Use expression body for method
+        _ = Assert.ThrowsExactly<MissingMethodException>(() =>
+            TestSmHasher("The quick brown fox jumps over the lazy dog"));
+#pragma warning restore IDE0022 // Use expression body for method
+    }
 
     /// <inheritdoc />
     /// <summary>
